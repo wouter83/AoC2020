ae8726823b94763522f172dff0b323832eade269
Wouter Kok <wouterkok@gmail.com>
2020-12-11 21:59:08 +0100

    OK, finally done...
    this is a mess, cleanup another time? Hope it wont bite me

--- src/day4.rs
+++ src/day4.rs
@@ -1,7 +1,11 @@
-trait Day4_trait {
-   fn is_valid(&self) -> bool;
+trait Day4Trait {
+  fn is_valid(&self) -> bool;
 }
 
+#[derive(Default)]
+struct Eyecollor {
+  color: String,
+}
 #[derive(Default)]
 struct Height {
   number: u16,
@@ -15,9 +19,36 @@ struct Passport {
   eyr: u16,
   hgt: Height,
   hcl: String,
-  ecl: String,
+  ecl: Eyecollor,
   pid: u32,
   cid: String,
+}
+
+impl Day4Trait for Height {
+  fn is_valid(&self) -> bool {
+    if self.metric == "cm" {
+      return self.number >= 150 && self.number <= 193;
+    }
+    if self.metric == "in" {
+      return self.number >= 59 && self.number <= 76;
+    }
+    false
+  }
+}
+
+impl Day4Trait for Eyecollor {
+  fn is_valid(&self) -> bool {
+    match self.color.as_str() {
+      "amb" => true,
+      "blu" => true,
+      "brn" => true,
+      "gry" => true,
+      "grn" => true,
+      "hzl" => true,
+      "oth" => true,
+      _ => false,
+    }
+  }
 }
 
 impl Passport {
@@ -45,12 +76,16 @@ impl Passport {
             ret.hgt.number = number_string.parse::<u16>().unwrap();
           }
           "hcl" => {
+            if key_val[1].len() != 7 {
+              break;
+            };
             let temp = key_val[1].find('#');
             if temp == Some(0) {
+              //              println!("hcl {:?}", key_val[1].as_bytes());
               let byte_array = key_val[1].as_bytes();
               let mut add = true;
               for i in 1..=6 {
-                add |= byte_array[i].is_ascii_digit();
+                add &= byte_array[i].is_ascii_hexdigit();
               }
               ret.hcl = if add {
                 key_val[1].to_string()
@@ -58,11 +93,15 @@ impl Passport {
                 "".to_string()
               };
             }
+            //            println!("ret.hcl: {}", ret.hcl);
           }
-          "ecl" => ret.ecl = key_val[1].to_string(),
+          "ecl" => ret.ecl.color = key_val[1].to_string(),
           "pid" => {
-            if key_val[1].to_string().len() <= 10 {
-              ret.pid = key_val[1].to_string().parse::<u32>().unwrap();
+            if key_val[1].to_string().len() == 9 {
+              let yess = key_val[1].to_string().parse::<u32>();
+              if !yess.is_err() {
+                ret.pid = yess.unwrap();
+              }
             }
           }
           "cid" => ret.cid = key_val[1].to_string(),
@@ -79,7 +118,7 @@ impl Passport {
     println!("eyr: {}", self.eyr);
     println!("hgt: {}{}", self.hgt.number, self.hgt.metric);
     println!("hcl: {}", self.hcl);
-    println!("ecl: {}", self.ecl);
+    println!("ecl: {}", self.ecl.color);
     println!("pid: {}", self.pid);
     println!("cid: {}", self.cid);
     println!("====");
@@ -88,13 +127,11 @@ impl Passport {
   fn is_valid(&self) -> bool {
     (self.byr >= 1920 && self.byr <= 2002)
       && (self.iyr >= 2010 && self.iyr <= 2020)
-      && (self.eyr >= 2010 && self.eyr <= 2030)
-      && (
-        if self.hgt.metric ==
-        self.hgt.number > 0)
-      && (self.hcl.len() > 0)
-      && (self.ecl.len() > 0)
-      && (self.pid > 0)
+      && (self.eyr >= 2020 && self.eyr <= 2030)
+      && (self.hgt.is_valid())
+      && (self.hcl.len() == 7)
+      && (self.ecl.is_valid())
+      && (self.pid > 0 && self.pid < 1000000000)
   }
 }
 
@@ -127,7 +164,7 @@ pub fn day4_main() {
       count_1 += 1;
     }
   }
-  println!("Day 4.1 awnser is: {}", count_1);
+  println!("Day 4.2 awnser is: {}", count_1);
 }
 
 #[test]
@@ -155,4 +192,60 @@ fn test_day4_passport_all() {
   assert_eq!(passp[1].is_valid(), false);
   assert_eq!(passp[2].is_valid(), true);
   assert_eq!(passp[3].is_valid(), false);
+}
+
+#[test]
+fn test_day4_passport_strict_invalid() {
+  let input = vec![
+    "eyr:1972 cid:100".to_string(),
+    "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926".to_string(),
+    "".to_string(),
+    "iyr:2019".to_string(),
+    "hcl:#602927 eyr:1967 hgt:170cm".to_string(),
+    "ecl:grn pid:012533040 byr:1946".to_string(),
+    "".to_string(),
+    "hcl:dab227 iyr:2012".to_string(),
+    "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277".to_string(),
+    "".to_string(),
+    "hgt:59cm ecl:zzz".to_string(),
+    "eyr:2038 hcl:74454a iyr:2023".to_string(),
+    "pid:3556412378 byr:2007".to_string(),
+    "".to_string(),
+    "iyr:2010 hgt:76in hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719".to_string(),
+  ];
+
+  let passp = parse_passports(&input);
+  assert_eq!(passp.len(), 5);
+
+  assert_eq!(passp[0].is_valid(), false);
+  assert_eq!(passp[1].is_valid(), false);
+  assert_eq!(passp[2].is_valid(), false);
+  assert_eq!(passp[3].is_valid(), false);
+  assert_eq!(passp[4].is_valid(), false);
+}
+
+#[test]
+fn test_day4_passport_strict_valid() {
+  let input = vec![
+    "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980".to_string(),
+    "hcl:#623a2f".to_string(),
+    "".to_string(),
+    "eyr:2029 ecl:blu cid:129 byr:1989".to_string(),
+    "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm".to_string(),
+    "".to_string(),
+    "hcl:#888785".to_string(),
+    "hgt:164cm byr:2001 iyr:2015 cid:88".to_string(),
+    "pid:545766238 ecl:hzl".to_string(),
+    "eyr:2022".to_string(),
+    "".to_string(),
+    "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719".to_string(),
+  ];
+
+  let passp = parse_passports(&input);
+  assert_eq!(passp.len(), 4);
+
+  assert_eq!(passp[0].is_valid(), true);
+  assert_eq!(passp[1].is_valid(), true);
+  assert_eq!(passp[2].is_valid(), true);
+  assert_eq!(passp[3].is_valid(), true);
 }
